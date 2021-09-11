cb935a2d12086c673c3d87468387b2b21ca173fb
Val Brisendine <vsbrisendine@gmail.com>
2021-09-11 09:43:37 -0500

    working storage

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
+    
+    // saved events
+        //LOCAL STORAGE for each save button to get the item (description's value) KEY
+        $("#9 .description").val(localStorage.getItem("9"));
+        $("#10 .description").val(localStorage.getItem("10"));
+        $("#11 .description").val(localStorage.getItem("11"));
+        $("#12 .description").val(localStorage.getItem("12"));
+        $("#13 .description").val(localStorage.getItem("13"));
+        $("#14 .description").val(localStorage.getItem("14"));
+        $("#15 .description").val(localStorage.getItem("15"));
+        $("#16 .description").val(localStorage.getItem("16"));
+        $("#17 .description").val(localStorage.getItem("17"));
+        // console.log("workin?");
+        time();
+    // ****************************************************
     $(".saveBtn").on("click", function () { //connected to storage "getItem"
         var textarea = $(this).siblings(".description").val(); //get the value of
         var time = $(this).parent().attr("id"); // in the hour id name 
-        localStorage.setItem(textarea, time);
+        localStorage.setItem(time, textarea); //KEY/VALUE - time/textarea
         //storage variables textarea & timeBlock
     })
 
     //DATE & TIME
     // currentTime = currentTime.startOf("hour"); DONE
-    
     var currentTime = moment();
         $('#currentTime').text(currentTime.format("MMM Do, YYYY, h:mm a"));
     // console.log("workin?"); DONE
@@ -18,9 +32,8 @@ $(document).ready(function () {
     function time() {
         //DONE
         //loop through time block to determine if it is past present future 
-
-        $(".time-block").each(function () {
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        $(".time-block").each(function () { //this pull from time-block class
+            var timeBlock = parseInt($(this).attr("id"));//.split("hour")[1]);
 
                 if (timeBlock < currentTime) {
                     $(this).toggleClass("past");
@@ -28,24 +41,10 @@ $(document).ready(function () {
                 else if (timeBlock === currentTime) {
                     $(this).toggleClass("present");
                 }
-                else { //if others fail, it must be future 
+                else { //if others false, it must be future 
                     $(this).toggleClass("future");
                 }
         })
     }
-    // -----------------------------------------------------------
-    // THEN the saved events persist
-    //LOCAL STORAGE for each save button to get the item (description's value) KEY
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    // console.log("workin?");
-    time();
 
 });
