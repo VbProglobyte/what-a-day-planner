d2b9ba45c4fa9440001d8efd9c4543c5b32e4116
Val Brisendine <vsbrisendine@gmail.com>
2021-09-10 23:41:07 -0500

    storage works but doesnt save to the screen

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $(".saveBtn").on("click", function () {
-        var textarea = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+    $(".saveBtn").on("click", function () { //connected to storage "getItem"
+        var textarea = $(this).siblings(".description").val(); //get the value of
+        var time = $(this).parent().attr("id"); // in the hour id name 
         localStorage.setItem(textarea, time);
         //storage variables textarea & timeBlock
     })
@@ -24,25 +24,18 @@ $(document).ready(function () {
 
                 if (timeBlock < currentTime) {
                     $(this).toggleClass("past");
-                    // $(this).removeClass("present");
-                    // $(this).removeClass("future");
                 }
-                if (timeBlock === currentTime) {
-                    // $(this).removeClass("past");
+                else if (timeBlock === currentTime) {
                     $(this).toggleClass("present");
-                    // $(this).removeClass("future");
                 }
-                else //(timeBlock > currentTime)
-                 {
-                    // $(this).removeClass("past");
-                    // $(this).removeClass("present");
+                else { //if others fail, it must be future 
                     $(this).toggleClass("future");
                 }
         })
     }
     // -----------------------------------------------------------
     // THEN the saved events persist
-    //LOCAL STORAGE for each save button to get the item (hour value)
+    //LOCAL STORAGE for each save button to get the item (description's value) KEY
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -52,7 +45,7 @@ $(document).ready(function () {
     $("#hour3 .description").val(localStorage.getItem("hour3"));
     $("#hour4 .description").val(localStorage.getItem("hour4"));
     $("#hour5 .description").val(localStorage.getItem("hour5"));
-    console.log("workin?");
+    // console.log("workin?");
     time();
 
 });
